e34e0f8880d1472ed3da77a367631b0d7d6cee6b
DayoTitiloye-code <DayoTitiloye@outlook.com>
2022-11-29 14:11:31 +0000

    Changes so far

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import InputForm from './components/InputForm'
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <InputForm/>
     </div>
   )
 }
---
+++ src/components/InputForm/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const InputForm = () =>{
+    return(
+        <form>
+            <label htmlFor="name"></label>
+            <input type="text" placeholder='Your Github username here...' id="name" />
+            <input type="submit" value="Search"/>
+        </form>
+    )
+}
+
+export default InputForm
