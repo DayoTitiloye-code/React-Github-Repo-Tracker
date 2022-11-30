3e48444d769f25d4b7026eeda72b3d0008887c33
DayoTitiloye-code <DayoTitiloye@outlook.com>
2022-11-30 09:53:25 +0000

    github icon

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -2,12 +2,19 @@ import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import InputForm from '../../components/InputForm'
 import GithubAPI from '../../components/GithubAPI'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
 
 const Home = () => {
     const[searchValue, setSearchValue] = useState('')
     return (
         <div className="home">
-            <h1>Welcome to Github Tracker</h1>
+            <div id="header">
+                <h1>Welcome to Github Tracker</h1>
+                <div id="icon">
+                    <FontAwesomeIcon icon={faGithub} />
+                </div>
+            </div>
             <InputForm setSearchValue={setSearchValue}/>
             <GithubAPI searchValue={searchValue}/>
         </div>
